6f4f405e2c39d1dde381b360a9d3bb48b3ed59dc
Marcelo8819 <silva.marcelohg@gmail.com>
2022-04-28 16:59:59 -0300

    first commit

---
+++ src/views/Home.views.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Home.view.css'
+
+function HomeView () {
+    return <div className="HomeView">
+        Olá, Mundo 2
+    </div>
+}
+
+export default HomeView
